1ef111d6cfef88cbe20afb0466cb2b0eb60d1de4
yubang <yubang@yubang-pc.(none)>
2015-05-29 10:16:25 +0800

    init

---
+++ backup.py
@@ -0,0 +1,34 @@
+#coding:UTF-8
+
+"""
+备份工具模块
+@author:yubang
+2015-05-29
+"""
+
+def buildMd5Lists(path):
+    "生成md5列表"
+    pass
+    
+def checkFileExistWarehouse(md5):
+    "检查本地文件仓库是否存在该md5文件"
+    pass
+    
+def copyFileFromWarehouse(targetPath,md5):
+    "从本地文件仓库复制文件到指定路径"
+    pass
+    
+def copyFileToWarehouse(sourcePath,md5):
+    "复制文件到文件仓库"
+    pass
+    
+def loginNewFile(md5):
+    "记录有新文件放入文件仓库，用于可以远程传输文件"
+    pass
+   
+def main():
+    "主函数"
+    pass
+    
+if __name__ == "__main__":
+    main()
